afe5aab1dbc6d4463ea2a522cb71f8b3a1c8123d
marcorichetta <marcorichetta@gmail.com>
2018-10-26 19:28:32 -0300

    Funciona el calculo de la cuota fija. Falta ver el redondeo

--- static/scripts.js
+++ static/scripts.js
@@ -2,8 +2,31 @@ function calcular() {
     let monto = document.getElementById("capital").value;
     let plazo = document.getElementById("plazo").value;
 
-    for (let i = 0; i < plazo; i++) {
-        let cuota = i;
-        console.log((monto/12)*i);
+    if (monto > 100000) {
+        alert("El monto debe ser menor a $100000");
     }
+
+    if (plazo > 72){
+        alert("El plazo debe ser menor a 72 meses");
+    }
+
+    document.getElementById("ResultadoCapital").innerText = cuotaFija();
+
+}
+
+function cuotaFija() {
+    let monto = document.getElementById("capital").value;
+    let plazo = document.getElementById("plazo").value;
+
+    let tna = 0.15;
+    let tem = tna / plazo;
+
+    let potencia = Math.pow(1 + tem, 12);
+
+    let cuota = monto * ((tem * potencia / ( potencia - 1 )));
+
+    cuota = Math.fround();
+    
+    Math.fround(cuota)
+    return cuota;
 }
