4f00a9190a7e461317a03a3abcbc8ca89e47db9f
marcorichetta <marcorichetta@gmail.com>
2018-10-29 23:32:01 -0300

    Terminé la pág de inicio y se pulió la de préstamo

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/plazo")
+def plazo():
+    return render_template("plazo.html")
+
+@app.route("/prestamo")
+def prestamo():
+    return render_template("prestamo.html")
--- static/scripts.js
+++ static/scripts.js
@@ -1,32 +1,69 @@
 function calcular() {
-    let monto = document.getElementById("capital").value;
-    let plazo = document.getElementById("plazo").value;
+    let capitalInicial = document.getElementById("capital").value;
+    let plazoMensual = document.getElementById("plazo").value;
 
-    if (monto > 100000) {
-        alert("El monto debe ser menor a $100000");
+    if (capitalInicial > 100000) {
+        alert("El capitalInicial debe ser menor a $100000");
     }
 
-    if (plazo > 72){
+    if (plazoMensual > 72){
         alert("El plazo debe ser menor a 72 meses");
     }
 
-    document.getElementById("ResultadoCapital").innerText = cuotaFija();
+    let cuotaFija = obtenerCuotaFija();
+    let iva = (cuotaFija * 21)/100;
+    let sumaTotal = cuotaFija + iva;
+    let ingresosNecesarios = sumaTotal * 3.33;
 
+    let tasaNominal = obtenerTasa() * 100;
+    let tasaEfectiva = tasaNominal / plazoMensual;
+
+    document.getElementById("ResultadoCapital").innerText = '$ ' + cuotaFija.toFixed(2);
+    document.getElementById("Iva").innerText = '$ ' + iva.toFixed(2);
+    document.getElementById("ResultadoIva").innerText = '$ ' + sumaTotal.toFixed(2);
+    document.getElementById("Ingresos").innerText = '$ ' + ingresosNecesarios.toFixed(2);
+
+    document.getElementById("TNA").innerText = tasaNominal.toFixed(2) + ' %';
+    document.getElementById("TEM").innerText = tasaEfectiva.toFixed(2) + ' %';
+}
+/*  Uso una función para obtener las tasas.
+    También uso una para setear este valor.
+*/
+function obtenerTasa() {
+    /**
+     * TNA: La tasa nominal anual (TNA) es el interés que nos pagan de manera
+     * anual (por un período de 12 meses) al colocar nuestro dinero en algún 
+     * instrumento financiero.
+     */
+    let tasaNominal = 0.56; // 0.17 === 17 %
+
+    return tasaNominal;
 }
 
-function cuotaFija() {
-    let monto = document.getElementById("capital").value;
-    let plazo = document.getElementById("plazo").value;
+// Tomo el capital y el plazo ingresados para calcular la cuota fija
+function obtenerCuotaFija() {
+    let capitalInicial = document.getElementById("capital").value;
+    let plazoMensual = document.getElementById("plazo").value;
 
-    let tna = 0.15;
-    let tem = tna / plazo;
+    let tasaNominal = obtenerTasa();
 
-    let potencia = Math.pow(1 + tem, 12);
+    /**
+     * TEA: La tasa efectiva anual (TEA) estipula la reinversión de los 
+     * intereses ganados al renovar en este caso un Plazo Fijo.
+     */
+    let tasaEfectiva = tasaNominal / plazoMensual;
 
-    let cuota = monto * ((tem * potencia / ( potencia - 1 )));
+    /**
+     * Guarda el resultado de (1+i)^n siendo n el plazo
+     */
+    let potencia = Math.pow(1 + tasaEfectiva, plazoMensual);
 
-    cuota = Math.fround();
-    
-    Math.fround(cuota)
-    return cuota;
+    /**
+     * Fórmula de Interes - Sistema Francés
+     * 
+     * C = S.[ {i. {(1+i)} ^ {n}} / {{(1+i)} ^ {n} -1} ]
+     */
+    let cuotaFija = capitalInicial * ((tasaEfectiva * potencia / ( potencia - 1 )));
+
+    return cuotaFija;
 }
