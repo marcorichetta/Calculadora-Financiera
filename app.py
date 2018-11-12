dc296304c4fda9e9489a416c7eac5b4550a09ef7
marcorichetta <marcorichetta@gmail.com>
2018-11-12 19:53:46 -0300

    Agregados Secret_key y flashing messages.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from cs50 import SQL
-from flask import Flask, flash, render_template, request, jsonify
+from flask import Flask, flash, render_template, request, jsonify, redirect
 
 app = Flask(__name__)
+
+app.secret_key = 'testing'
 
 db = SQL("sqlite:///prueba.db")
 
@@ -32,17 +34,23 @@ def solicitud():
         # Data to be sent to database.
         userData = [nombre,apellido,localidad, telefono, email]
         print(userData)
+
+        flash('El plazo fijo fue depositado con éxito')
+        
         db.execute("INSERT INTO usuarios (usuario, telefono, domicilio, sueldo) VALUES (:usuario, :telefono, :domicilio, :sueldo)",
                     usuario=nombre,
                     telefono=telefono,
                     domicilio=localidad,
                     sueldo=45000)
 
-        return render_template("index.html")
+        return redirect("/")
     else:
+
+        # Tomo las variables de la url
         capital = request.args.get("capital")
         plazo = int(request.args.get("plazo"))
         
+        # Calculo tasa para pasarle al template de la solicitud
         if (plazo < 45):
             tasa = 48
         elif (plazo < 60):
@@ -56,6 +64,8 @@ def solicitud():
 
 @app.route("/obtenerTasa")
 def obtenerTasa():
+    
+    # Consulta ajax para obtener la tasa en el calculo del plazo fijo
     dias = int(request.args.get("plazo"))
     
     if (dias < 45):
