c02bb4e32bf4b97f571a1589240c6b024acb0351
Marco Richetta <marcorichetta@gmail.com>
2019-05-31 11:56:01 -0300

    Se agregaron statements para crear tasas
    Insert tasas de PF y Préstamo

--- creacionDB.sql
+++ creacionDB.sql
@@ -21,11 +21,22 @@ CREATE TABLE "tasasPF" (
         `dias`  INTEGER NOT NULL,
         `tasa`  NUMERIC NOT NULL
 );
+-- Insertar 3 tasas Plazo Fijo(id, días, % interés)
+-- TODO - Form ABM
+INSERT INTO tasasPF VALUES(1,60,48);
+INSERT INTO tasasPF VALUES(2,120,47);
+INSERT INTO tasasPF VALUES(3,365,45);
+
 CREATE TABLE "tasasPrest" ( 
         `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
         `meses` INTEGER NOT NULL, 
         `tasa` NUMERIC NOT NULL
 );
+-- Insertar 3 tasas Préstamo(id, meses, % interés)
+INSERT INTO tasasPrest VALUES(1,24,56);
+INSERT INTO tasasPrest VALUES(2,48,65);
+INSERT INTO tasasPrest VALUES(3,72,75);
+
 CREATE TABLE "prestamos" (
         `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         `dnicliente`    INTEGER NOT NULL,
@@ -35,3 +46,7 @@ CREATE TABLE "prestamos" (
         `sistema`  INTEGER NOT NULL,
         `fechaSolicitud`        DATETIME DEFAULT CURRENT_TIMESTAMP
 )
+DELETE FROM sqlite_sequence;
+-- Actualizar índices de las tablas modificadas
+INSERT INTO sqlite_sequence VALUES('tasasPF',3);
+INSERT INTO sqlite_sequence VALUES('tasasPrest',3);
